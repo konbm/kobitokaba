a66c5ac2ff07a7db050328105dd8bb64ef138bc5
konbm <p.load.fffff@gmail.com>
2018-11-20 12:42:14 +0900

    再修正

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 'use strict';
 window.onload=function(){
-	var images=document.getElementsByClassName("heaerImage");
+	var images=document.getElementsByClassName("headerImage");
 	var zIndexMax=0;
 	for(var i=0;i<images.length;i++){
 		images[i].addEventListener("Click",function(){
